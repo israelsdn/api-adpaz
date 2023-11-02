e4738a3bd4588b7cf87e84d3b777e7d99074e6b7
Israel Sales <israelsales043@gmail.com>
2023-11-01 21:56:39 -0300

    node-vercel

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { router } from './router';
 import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+app.listen(process.env.PORT);
